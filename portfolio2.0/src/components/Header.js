29043b739fa73d498b58d6005415f19cdc6e7231
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2020-12-09 17:55:40 +0000

    Header created with pre element and typewriter description

---
+++ portfolio2.0/src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Typical from 'react-typical'
+
+const Header = () => {
+    var title = String.raw`
+ _____    __          __                               __
+|  _  \  |__|        |  |                             |  |
+| | \  \  __   ____  |  |____   ________  ______ _____|  |
+| |_/  / |  | /  _ \ |   __  \ /   __   ||   __//   __   |
+|  _  /  |  | | | \/ |  |  |  ||  |  |  ||  |   |  |  |  |
+| | \ \  |  | | |_/\ |  |  |  ||  |__|  ||  |   |  |__|  |
+|_|  \_\ |__| \____/ |__|  |__|\______,_||__|   |________|
+
+––           __       __           __        __    
+\  \        /  /     |__|         |  |      |  |   
+ \  \      /  /______ __  ______  |  |_____ |  |__ 
+  \  \    /  /|   __/|  |/   _   ||   __   \|   __|
+   \  \/\/  / |  |   |  ||  / \  ||  |  |  ||  |   
+    \      /  |  |   |  ||  \_/  ||  |  |  ||  |__ 
+     \_/\_/   |__|   |__|\____,  ||__|  |__|\____| 
+                          ____|  |                  
+                         |______/                   
+        `;
+
+        return (
+        <header>
+             <img id="profile_pic"src="https://i.imgur.com/pm1X1BP.png?1" alt="Profile pic"/>
+             <div id='name'>
+                <pre>
+                    {title}
+                </pre>
+             </div> 
+            <p id="describe">I am a{' '}   
+                <Typical loop={1} wrapper= 'b' steps={['Web Developer', 1000, 'Designer', 1000, 'Software Engineer.', 1000]}/></p>
+        </header>
+        )
+}
+
+
+export default Header;
