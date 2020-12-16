54ec0c7ebfb00724d9a6457b86c3e7a65b022768
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2020-12-16 14:07:59 +0000

    added responsive styles to sections for smaller screens

--- portfolio2.0/src/components/Footer.js
+++ portfolio2.0/src/components/Footer.js
@@ -4,10 +4,10 @@ import React from 'react'
 const Footer = () => {
     return (
         <div id='footer'>
-            <a href='https://github.com/Richie-1876'target="_blank" rel='noreferrer'><i className="fa fa-github fa-5x" aria-hidden="true"></i></a>
-            <a href='https://www.linkedin.com/in/richard-wright1876/'target="_blank" rel='noreferrer'><i class="fa fa-linkedin fa-5x" aria-hidden="true"></i></a>
-            <a href='https://www.instagram.com/blingbong1876/'target="_blank" rel='noreferrer'><i class="fa fa-instagram fa-5x" aria-hidden="true"></i></a>
-            <a href='https://www.facebook.com/richard.wright.3576'target="_blank" rel='noreferrer'><i class="fa fa-facebook fa-5x" aria-hidden="true"></i></a>
+            <a href='https://github.com/Richie-1876'target="_blank" rel='noreferrer'><i className="fa fa-github fa-5x" aria-hidden="true"> </i> </a>
+            <a href='https://www.linkedin.com/in/richard-wright1876/'target="_blank" rel='noreferrer'><i className="fa fa-linkedin fa-5x" aria-hidden="true"> </i> </a>
+            <a href='https://www.instagram.com/blingbong1876/'target="_blank" rel='noreferrer'><i className="fa fa-instagram fa-5x" aria-hidden="true"> </i> </a>
+            <a href='https://www.facebook.com/richard.wright.3576'target="_blank" rel='noreferrer'><i className="fa fa-facebook fa-5x" aria-hidden="true"> </i> </a>
         </div>
     )
 }
--- portfolio2.0/src/components/Projects.js
+++ portfolio2.0/src/components/Projects.js
@@ -22,42 +22,42 @@ const Projects = () => {
             </pre>
             </div>
             <div id='projects-container'>
-            <div class="project">
+            <div className="project">
                 <h2>Pedal Board App</h2>
                 <a href="https://pedal-board.herokuapp.com/"target="_blank" rel='noreferrer'><img src="https://i.imgur.com/vNE6tfE.png"alt="Artist"/></a>
                 <span>Click Image to visit live site</span>
                 <p>Community Pedal Board app built using React, Bootstrap, Flexbox, Javascript, postgresQL, Python, and Django. Many to many relationship between models.</p>
             </div>
 
-            <div class="project">
+            <div className="project">
                 <h2>Rain Dogs Society</h2>
                 <a href="http://www.raindogssociety.com/"target="_blank" rel='noreferrer'><img src="https://i.imgur.com/nVgQuar.png"alt="Homepage"/></a>
                 <span>Click Image to visit live site</span>
                 <p>Homepage site created for a small business in Glasgow. Links to booking app and social media pages. Built with HTML, CSS, Bootstrap, flexbox and javaScript.</p>
             </div>
 
-            <div class="project">
+            <div className="project">
                 <h2>Ecommerce App</h2>
                 <a href="https://react-ecommerce-demo-app.herokuapp.com/"target="_blank" rel='noreferrer'><img src="https://i.imgur.com/tPT5xYK.png"alt="Cards"/></a>
                 <span>Click Image to visit live site</span>
                 <p>Ecommerce App built using React, Hooks, GraphQl, ContextAPI, stripe for payment integration, and a Firebase DB for Auth and storage.</p>
             </div>
 
-            <div class="project">
+            <div className="project">
                 <h2>Higher/Lower App</h2>
                 <a href="https://richie-1876.github.io/project_1/higher_lower_app/#"target="_blank" rel='noreferrer'><img src="https://i.imgur.com/mPmd8sO.png"alt="Cards"/></a>
                 <span>Click Image to visit live site</span>
                 <p>Higher or lower card game built with the use of a card API through an AJAX request, using HTML, CSS, jQuery and Javascript.</p>
             </div>
 
-            <div class="project">
+            <div className="project">
                 <h2>My Recipes App</h2>
                 <a href="https://rwright-express-mongo-example.herokuapp.com/recipes"target="_blank" rel='noreferrer'><img src="https://i.imgur.com/f4nubjY.png"alt="Recipes"/></a>
                 <span>Click Image to visit live site</span>
                 <p>Community recipe app using Javascript, MongoDB, Mongoose, Express and EJS.</p>
             </div>
 
-            <div class="project">
+            <div className="project">
                 <h2>Cheers App</h2>
                 <a href="https://cheers-frontend.herokuapp.com/"target="_blank" rel='noreferrer'><img src="https://i.imgur.com/BJs0qpO.png"alt="Beers"/></a>
                 <span>Click Image to visit live site</span>
